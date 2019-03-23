bc7b9eee4472e8ca915b9c24c5384aeb6d1bfc86
jn01674361 <joar.nykvist@gmail.com>
2019-03-23 11:01:48 +0100

    Add files

--- main.go
+++ main.go
@@ -60,7 +60,7 @@ func DeletePerson(w http.ResponseWriter, r *http.Request) {
 // our main function
 func main() {
     router := mux.NewRouter()
-    people = append(people, Person{ID: "1",g Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
+    people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
     people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
     people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
     router.HandleFunc("/people", GetPeople).Methods("GET")
